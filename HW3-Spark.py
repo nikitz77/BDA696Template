67076f40b11420b744627e3d0163e0ccdb2323ff
nikitz77 <nikazaballa@gmail.com>
2020-10-03 18:08:04 -0700

    do HW3

--- HW3-Spark.py
+++ HW3-Spark.py
@@ -1,25 +1,32 @@
-# Setup Spark
-
+import sys
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master("local[*]").getOrCreate()
+# Setup Spark
 
-database = "baseball_db"
-port = "3306"
-user = "root"  # pragma: allowlist secret
-password = "x11docker"  # pragma: allowlist secret
 
-df = (
-    spark.read.format("jdbc")
-    .options(
-        url=f"jdbc:mysql://localhost:{port}/{database}",
-        driver="com.mysql.cj.jdbc.Driver",
-        dbtable="batter_counts",
-        user=user,  # pragma: allowlist secret
-        password=password,  # pragma: allowlist secret
+def main():
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+    database = "baseball_db"
+    port = "3306"
+    user = "root"  # pragma: allowlist secret
+    password = "x11docker"  # pragma: allowlist secret
+
+    df = (
+        spark.read.format("jdbc")
+        .options(
+            url=f"jdbc:mysql://localhost:{port}/{database}",
+            driver="com.mysql.cj.jdbc.Driver",
+            dbtable="batter_counts",
+            user=user,  # pragma: allowlist secret
+            password=password,  # pragma: allowlist secret
+        )
+        .load()
     )
-    .load()
-)
 
-df.show()
+    df.show()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
